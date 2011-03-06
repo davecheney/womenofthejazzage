e8ca88a1f2e90d04de21ec0d1e6c4c0a37f092d4
Dave Cheney <dave@cheney.net>
2011-03-06 15:37:18 +1100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Womenofthejazzage::Application.config.secret_token = '17fe80310c6ec2083d14606d70b83de905f2c87396d0bcc61b465df3596035088f9d5fed9a661eca4ad65840b998063c01caad46663a7e87ac4588e50a7e98e7'
